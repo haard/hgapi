b68f46443ca56df06a60e886057cd652860ec392
nkj <none@none>
2012-03-30 18:42:46 +0200

    no need for dqueue

--- hgapi/hgapi.py
+++ hgapi/hgapi.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, unicode_literals, with_statement
 from subprocess import Popen, STDOUT, PIPE
-from collections import deque
 try:
     from urllib import unquote
 except: #python 3
